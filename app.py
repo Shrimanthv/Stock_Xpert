6b26eaf59d397a9ee5cf4facbcbf2c9e788044df
Shrimanthv <shrimanthv99@gmail.com>
2025-07-18 20:33:21 +0530

    Updated my Readme file.

--- app.py
+++ app.py
@@ -1,41 +1,4 @@
-# import streamlit as st
-# import pandas as pd
-# import numpy as np
-# from sklearn.ensemble import RandomForestClassifier
-# from sklearn.model_selection import GridSearchCV
-# import pickle
 
-
-# st.title("Stock Price Prediction System")
-
-# with open('random_forest_model.pkl', 'rb') as file:
-#     best_rf_model = pickle.load(file)
-    
-    
-# st.header("Enter Stock Data for Prediction")
-
-# open_price = st.number_input("Open Price", min_value=0.0, format="%.2f")
-# high_price = st.number_input("High Price", min_value=0.0, format="%.2f")
-# low_price = st.number_input("Low Price", min_value=0.0, format="%.2f")
-# close_price = st.number_input("Close Price", min_value=0.0, format="%.2f")
-# volume = st.number_input("Volume", min_value=0, format="%d")
-
-# if st.button("Predict"):
-#     input_data = pd.DataFrame({
-#         "Open": [open_price],
-#         "High": [high_price],
-#         "Low": [low_price],
-#         "Close": [close_price],
-#         "Volume": [volume]
-#     })
-#     prediction = best_rf_model.predict(input_data)[0]
-#     st.write("Prediction Result:",prediction)
-#     if prediction==1:
-#         st.success("The stock price is likely to go up tomorrow.")
-#     else:
-#         st.warning("The stock price is likely to go down tomorrow.")
-    
-    
 import streamlit as st
 import numpy as np
 import pickle
@@ -44,7 +7,7 @@ import pickle
 with open("random_forest_model.pkl", "rb") as f:
     model = pickle.load(f)
 
-st.title("Stock Movement Predictor")
+st.title("📈  StockXpert — Stock Movement Prediction using Random Forest Classifier")
 
 st.write("Enter the values for the features below:")
 
